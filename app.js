9198e8b6b8484bdeb8b0703ad7fe1752df4462bd
victor.duclos <victor.duclos@groupe-esigelec.org>
2020-06-05 23:39:38 +0300

    fix heroku crash

--- app.js
+++ app.js
@@ -51,10 +51,24 @@ client.on('message', msg => {
             break;
     }
 
-    if(msg.content[0] ===prefixCall){
+    if (msg.content[0] === prefixCall) {
         msg.delete();
     }
 });
 
 
 client.login(process.env.DISCORD_TOKEN).then(r => console.log('Token is good, connected!'));
+
+
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function (request, response) {
+    const result = 'App is running';
+    response.send(result);
+}).listen(app.get('port'), function () {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
