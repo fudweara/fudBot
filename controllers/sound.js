1d53fa4298641859cccf41706399c6ae54bd4fc2
victor.duclos <victor.duclos@groupe-esigelec.org>
2020-06-06 15:36:58 +0300

    add sound name to db

--- controllers/sound.js
+++ controllers/sound.js
@@ -3,27 +3,36 @@ const mongo = require('../config/mongo');
 const request = require('request').defaults({encoding: null});
 
 addNewSound = (msg) => {
+    const soundName = msg.content.split(' ')[1];
 
-    if (msg.attachments.size) {
+    // Check if something is missing or sound size is too big
+    if (!soundName) {
+        msg.reply('La commande c\'est !add nomduson et toi tu as oublié le nom du son');
+        return;
+    }
 
-        if (msg.attachments.first().size > 2000000) {
-            msg.reply('Fichier audio trop grand. La taille maximum est 2mo');
-            return;
-        }
+    if (!msg.attachments.size) {
+        msg.reply('Il manque le son');
+        return;
+    }
 
-        request.get(msg.attachments.first().url, function (err, res, body) {
+    if (msg.attachments.first().size > 2000000) {
+        msg.reply('Fichier audio trop grand. La taille maximum est 2mo');
+        return;
+    }
 
-            mongo.addSound({
-                binary: body,
-                author: msg.author.id,
-                createdAt: new Date(),
-            });
+    // We can add the sound to the DB
+    request.get(msg.attachments.first().url, function (err, res, body) {
+
+        mongo.addSound({
+            name: soundName,
+            binary: body,
+            author: msg.author.id,
+            createdAt: new Date(),
         });
+    });
 
-        console.log('file uploaded')
-    } else
-        msg.reply('Il manque le son')
-
+    console.log('file uploaded')
 
 };
 
